af3e5fdfa4b5888b696c40269ee8fcd25b418087
tracer-nju <tracer@nju.edu.cn>
2021-03-09 08:36:37 -0500

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: cd6a83b3241a4e3d8dd7a1cb30b89e41     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  08:36:37 up  3:52,  2 users,  load average: 0.16, 0.08, 0.03

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[]) {
     printf("argv[%d] = %s\n", i, argv[i]);
   }
   assert(!argv[argc]);
-  Dir *dir = opendir("/proc");
+  DIR *dir = opendir("/proc");
   assert(dir != NULL);
   return 0;
 }
