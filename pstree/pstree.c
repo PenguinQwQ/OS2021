87c1e697fafeb3a36f7cbfd5a3d63bdac1d46468
tracer-nju <tracer@nju.edu.cn>
2021-03-09 08:46:10 -0500

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: cd6a83b3241a4e3d8dd7a1cb30b89e41     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  08:46:10 up  4:01,  3 users,  load average: 0.19, 0.21, 0.12

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -2,13 +2,16 @@
 #include <dirent.h>
 #include <assert.h>
 
+char path[256] = {"/proc"};
+
 int main(int argc, char *argv[]) {
   for (int i = 0; i < argc; i++) {
     assert(argv[i]);
     printf("argv[%d] = %s\n", i, argv[i]);
   }
   assert(!argv[argc]);
-  DIR *dir = opendir("/proc");
+  strcmp(path, "")
+  DIR *dir = opendir(path);
   assert(dir != NULL);
   struct dirent *ptr;
   while ((ptr = readdir(dir)) != NULL) {
