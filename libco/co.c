b54a52b4d556ffd73b2bfe49056acc14d53d39ce
tracer-nju <tracer@nju.edu.cn>
2021-03-15 10:04:25 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: cd6a83b3241a4e3d8dd7a1cb30b89e41     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  10:04:25 up 1 day, 13:35,  7 users,  load average: 0.06, 0.22, 0.17

--- libco/co.c
+++ libco/co.c
@@ -63,7 +63,8 @@ struct co *co_start(const char *name, void (*func)(void *), void *arg) {
 static inline void stack_switch_call (void *sp, void *entry, uintptr_t arg) {
 	  asm volatile (
 	  #if __x86_64__
-	      "movq %0, %%rsp; movq %2, %%rdi; jmp *%1"
+	      "andq $0xfffffffffffffff0, %%rsp;
+		   movq %0, %%rsp; movq %2, %%rdi; jmp *%1"
 		     : : "b"((uintptr_t)sp),     "d"(entry), "a"(arg)
 	  #else
 		  "movl %0, %%esp; movl %2, 4(%0); jmp *%1"
