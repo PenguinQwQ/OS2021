2f4d862a9bf8765943569afc2cafb020e4f7435d
tracer-nju <tracer@nju.edu.cn>
2021-03-15 10:15:24 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: cd6a83b3241a4e3d8dd7a1cb30b89e41     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  10:15:25 up 1 day, 13:46,  7 users,  load average: 0.24, 0.21, 0.18

--- libco/co.c
+++ libco/co.c
@@ -41,9 +41,9 @@ __attribute__((constructor)) static void init() {
 	now -> func = NULL;
 	now -> arg  = NULL;
 
-	now -> status = CO_RUNNING;
-	now -> waiter = NULL;
-	
+	now -> status  = CO_RUNNING;
+	now -> waiter  = NULL;
+	now -> context = NULL;
 	cor[sum++] = now;
 }
 
@@ -54,16 +54,18 @@ struct co *co_start(const char *name, void (*func)(void *), void *arg) {
 	now -> func = func;
 	now -> arg  = arg;
 
-	now -> status = CO_NEW;
-	now -> waiter = NULL;
+	now -> status  = CO_NEW;
+	now -> waiter  = NULL;
+	now -> context = NULL;
 	memset(now -> stack, 0, sizeof(now -> stack));
 	cor[sum++] = now;
+	cur = now;
 	return now;
 }
 static inline void stack_switch_call (void *sp, void *entry, uintptr_t arg) {
 	  asm volatile (
 	  #if __x86_64__
-	      "andq $0xfffffffffffffff0, %%rsp; movq %0, %%rsp; movq %2, %%rdi; jmp *%1"
+	      "movq %0, %%rsp; andq $0xfffffffffffffff0, %%rsp;movq %2, %%rdi; jmp *%1"
 		     : : "b"((uintptr_t)sp),     "d"(entry), "a"(arg)
 	  #else
 		  "movl %0, %%esp; movl %2, 4(%0); jmp *%1"
@@ -75,8 +77,20 @@ static inline void stack_switch_call (void *sp, void *entry, uintptr_t arg) {
 
 
 void co_yield() {
-	
-	
+	int val = setjmp(cur -> context);
+	if (val == 0) {
+		int id = rand() % sum;
+		cur = cor[id];	
+		if (cur -> context == NULL) {
+			stack_switch_call(&cur->stack[MAX_SIZE], cur->func, cur->arg);
+		}
+		else {
+			longjmp(cur->context);	
+		}
+	}
+	else {
+		return;	
+	}
 }
 
 void co_wait(struct co *co) {
