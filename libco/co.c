eb38226669dc1c81388eab716af2ff914a2dc109
tracer-nju <tracer@nju.edu.cn>
2021-03-15 09:44:25 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: cd6a83b3241a4e3d8dd7a1cb30b89e41     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  09:44:25 up 1 day, 13:15,  7 users,  load average: 0.08, 0.08, 0.08

--- libco/co.c
+++ libco/co.c
@@ -1,15 +1,52 @@
 #include "co.h"
 #include <stdlib.h>
 
-struct co {
-};
+#define STACK_SIZE 65536
+#define MAX_SIZE   128
 
-struct co *co_start(const char *name, void (*func)(void *), void *arg) {
-  return NULL;
+enum {
+	CO_NEW = 1,
+	CO_RUNNING,
+	CO_WAITING,
+	CO_DEAD,	
+	
 }
 
-void co_wait(struct co *co) {
+struct co {
+	char *name;
+	void (*func)(void *);
+	void *arg;
+
+	enum    co_status status;
+	struct  co* waited;
+	jmp_buf context;
+	uint8_t stack[STACK_SIZE];
+};
+
+struct co* cor[MAX_SIZE];
+
+struct co *cur;
+
+static int sum = 0;
+
+struct co *co_start(const char *name, void (*func)(void *), void *arg) {
+	struct co *now = (struct co *)malloc(sizeof(co));
+	assert(co != NULL);
+	now -> name = name;
+	now -> func = func;
+	now -> arg  = arg;
+
+	now -> status = CO_NEW;
+	now -> waited = NULL;
+	memset(now -> stack, 0, sizeof(now -> stack));
+	cor[sum++] = co;
 }
 
 void co_yield() {
+	
+	
+}
+
+void co_wait(struct co *co) {
+
 }
