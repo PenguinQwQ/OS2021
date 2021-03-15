d8fdd9d33f6f31100ebea22348a2f3745311869a
tracer-nju <tracer@nju.edu.cn>
2021-03-15 10:01:30 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: cd6a83b3241a4e3d8dd7a1cb30b89e41     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  10:01:30 up 1 day, 13:33,  7 users,  load average: 0.43, 0.34, 0.20

--- libco/co.c
+++ libco/co.c
@@ -60,6 +60,19 @@ struct co *co_start(const char *name, void (*func)(void *), void *arg) {
 	cor[sum++] = now;
 	return now;
 }
+static inline void stack_switch_call (void *sp, void *entry, uintptr_t arg) {
+	  asm volatile (
+	  #if __x86_64__
+	      "movq %0, %%rsp; movq %2, %%rdi; jmp *%1"
+		     : : "b"((uintptr_t)sp),     "d"(entry), "a"(arg)
+	  #else
+		  "movl %0, %%esp; movl %2, 4(%0); jmp *%1"
+			 : : "b"((uintptr_t)sp - 8), "d"(entry), "a"(arg)
+      #endif
+	  );
+}
+
+
 
 void co_yield() {
 	
