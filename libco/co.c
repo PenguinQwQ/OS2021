1d570479c4d3e1b44d2c3d79bb70e1e205a9f164
tracer-nju <tracer@nju.edu.cn>
2021-03-16 04:00:03 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: cd6a83b3241a4e3d8dd7a1cb30b89e41     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  04:00:03 up 1 day, 16:26,  8 users,  load average: 0.19, 0.16, 0.10

--- libco/co.c
+++ libco/co.c
@@ -65,10 +65,10 @@ struct co *co_start(const char *name, void (*func)(void *), void *arg) {
 static inline void stack_switch_call (void *sp, void *entry, uintptr_t arg, void *entry2) {
 	  asm volatile (
 	  #if __x86_64__
-	      "movq %0, %%rsp; andq $0xfffffffffffffff0, %%rsp;movq %2, %%rdi;            pushq *%3; callq *%1"
+	      "movq %0, %%rsp; andq $0xfffffffffffffff0, %%rsp;movq %2, %%rdi;            pushq %3; callq *%1"
 		     : : "b"((uintptr_t)sp),     "d"(entry), "a"(arg), "c"(entry2)
 	  #else
-		  "movl %0, %%esp; movl %2, 4(%0); call *%1"
+		  "movl %0, %%esp; movl %2, 4(%0); push %3; call *%1"
 			 : : "b"((uintptr_t)sp - 8), "d"(entry), "a"(arg), "c"(entry2)
       #endif
 	  );
