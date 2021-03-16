172ab542d9f00e1abcca990d74b9521682a06bb5
tracer-nju <tracer@nju.edu.cn>
2021-03-16 07:40:46 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: cd6a83b3241a4e3d8dd7a1cb30b89e41     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  07:40:46 up 1 day, 17:45,  7 users,  load average: 0.32, 0.16, 0.11

--- libco/test1/main.c
+++ libco/test1/main.c
@@ -15,8 +15,7 @@ void entry(void *arg) {
 int main() {
 	struct co *co1 = co_start("co1", entry, "a");
 	struct co *co2 = co_start("co2", entry, "b");
-	co_wait(co1);
-	co_wait(co2);
+	co_yield();
 	printf("Done\n");
 	return 0;	
 }
