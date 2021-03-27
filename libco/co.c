7d8729923ec8715e9490128c5b3a5b1d056e7a4e
tracer-nju <tracer@nju.edu.cn>
2021-03-27 03:55:16 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: e55ec371cf7c48fa81f6ec3af0544255     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  03:55:16 up  2:20,  7 users,  load average: 1.37, 1.28, 1.20

--- libco/co.c
+++ libco/co.c
@@ -91,7 +91,7 @@ void co_yield() {
 		int id = rand() % sum;
 		while (cor[id] -> status == CO_WAITING || (id==0 && sum != 1)) {
 			id = rand() % sum;
-			printf("%d\n");
+			printf("%d\n", id);
 		}
 		cur = cor[id];
 		if (cur -> status == CO_NEW) {
