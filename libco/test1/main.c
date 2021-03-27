5a5b9a7b283c809c25583a82ddac54bd5812fc27
tracer-nju <tracer@nju.edu.cn>
2021-03-27 00:44:33 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 1bb35f7886a04f0e8ac930abfce21317     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  00:44:34 up  1:02,  3 users,  load average: 0.30, 0.26, 0.15

--- libco/test1/main.c
+++ libco/test1/main.c
@@ -2,16 +2,126 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <string.h>
-#include <co.h>
+#include "co-test.h"
 
-int count = 1;
+int g_count = 0;
 
-void entry(void *arg) {
-	printf("1\n");
+static void add_count() {
+    g_count++;
 }
+
+static int get_count() {
+    return g_count;
+}
+
+static void work_loop(void *arg) {
+        co_yield();
+		printf("%s\n",arg);
+		co_yield();
+		printf("%s\n",arg);
+}
+
+static void work(void *arg) {
+    work_loop(arg);
+}
+
+static void test_1() {
+
+    struct co *thd1 = co_start("thread-1", work, "X");
+    struct co *thd2 = co_start("thread-2", work, "Y");
+    co_wait(thd1);
+    co_wait(thd2);
+
+//    printf("\n");
+}
+
+// -----------------------------------------------
+
+static int g_running = 1;
+
+static void do_produce(Queue *queue) {
+    assert(!q_is_full(queue));
+    Item *item = (Item*)malloc(sizeof(Item));
+    if (!item) {
+        fprintf(stderr, "New item failure\n");
+        return;
+    }
+    item->data = (char*)malloc(10);
+    if (!item->data) {
+        fprintf(stderr, "New data failure\n");
+        free(item);
+        return;
+    }
+    memset(item->data, 0, 10);
+    sprintf(item->data, "libco-%d", g_count++);
+    q_push(queue, item);
+}
+static void producer(void *arg) {
+    Queue *queue = (Queue*)arg;
+    for (int i = 0; i < 100; ) {
+        if (!q_is_full(queue)) {
+            // co_yield();
+            do_produce(queue);
+            i += 1;
+        }
+        co_yield();
+    }
+}
+
+static void do_consume(Queue *queue) {
+    assert(!q_is_empty(queue));
+
+    Item *item = q_pop(queue);
+    if (item) {
+        printf("%s  ", (char *)item->data);
+        free(item->data);
+        free(item);
+    }
+}
+
+static void consumer(void *arg) {
+    Queue *queue = (Queue*)arg;
+    while (g_running) {
+        if (!q_is_empty(queue)) {
+            do_consume(queue);
+        }
+        co_yield();
+    }
+}
+
+static void test_2() {
+
+    Queue *queue = q_new();
+
+    struct co *thd1 = co_start("producer-1", producer, queue);
+    struct co *thd2 = co_start("producer-2", producer, queue);
+    struct co *thd3 = co_start("consumer-1", consumer, queue);
+    struct co *thd4 = co_start("consumer-2", consumer, queue);
+
+    co_wait(thd1);
+    co_wait(thd2);
+
+    g_running = 0;
+
+    co_wait(thd3);
+    co_wait(thd4);
+
+    while (!q_is_empty(queue)) {
+        do_consume(queue);
+    }
+
+    q_free(queue);
+}
+
 int main() {
-	struct co *co1 = co_start("co1", entry, "a");
-	co_yield();
-	printf("Done\n");
-	return 0;	
+    setbuf(stdout, NULL);
+
+    printf("Test #1. Expect: (X|Y){0, 1, 2, ..., 199}\n");
+    test_1();
+
+    printf("\n\nTest #2. Expect: (libco-){200, 201, 202, ..., 399}\n");
+
+    printf("\n\n");
+
+    return 0;
 }
