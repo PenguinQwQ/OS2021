bc6e47b209f2b619d690350d8756a8df52cd9312
tracer-nju <tracer@nju.edu.cn>
2021-03-30 13:57:31 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: e0386af4c1124b03894b65904ec0a97c     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  13:57:31 up 17:49,  5 users,  load average: 0.32, 0.28, 0.27

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -42,11 +42,12 @@ int judge_size(size_t size) {
 void* deal_slab(int id, int kd) {
 	struct page_t *now;
 	now = page_table[id][kd];
-	printf("%d %d %d\n", id, kd, now->remain);
 	while (now != NULL && now -> remain == 0) now = now ->next;
 	assert(now != NULL);
 	assert(now -> remain != 0);
-    return (void *)_ptr[now -> belong][-- now -> remain];	
+	now -> remain--;
+	printf("%d %d %d\n", id, kd, now->remain);
+    return (void *)_ptr[now -> belong][now -> remain];	
 }
 
 void deal_slab_free(struct page_t *now, void *ptr) {
