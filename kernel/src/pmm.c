142234417933d813d2dee7ad0302c3cf5493427c
tracer-nju <tracer@nju.edu.cn>
2021-03-30 10:21:08 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: e0386af4c1124b03894b65904ec0a97c     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  10:21:08 up 15:37,  4 users,  load average: 0.42, 0.23, 0.13

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -1,16 +1,60 @@
 #include <common.h>
+#define PAGE_SIZE      8192
+#define MAX_CPU        8
+#define MAX_DATA_SIZE  5
+#define MAX_PAGE       100
+
+typedef struct{
+	int flag;	
+}spinlock_t;
+
+uintptr_t ptr[MAX_PAGE][512];
+int		  sum[MAX_PAGE] = {0};
+
+
+int DataSize[MAX_DATA_SIZE] = {16, 32, 64, 128};
+
+struct page_t{
+	spinlock_t *lock;
+	struct page_t *next;
+	int block_size;
+	int belong;
+};
+
+struct page_t *page_table[MAX_CPU][MAX_DATA_SIZE];
+spinlock_t lock[MAX_CPU];
 
 static void *kalloc(size_t size) {
-  printf("%d\n", cpu_count());
   return NULL;
 }
 
 static void kfree(void *ptr) {
 }
 
+
+
 static void pmm_init() {
   uintptr_t pmsize = ((uintptr_t)heap.end - (uintptr_t)heap.start);
+  heap.start = (void *)ROUNDUP(heap.start, PAGE_SIZE);
   printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);
+  
+  int tot = cpu_count(), cnt = 0;
+  for (int i = 0; i < tot; i++) {
+	  for (int j = 0; j < MAX_DATA_SIZE; j++) {
+		 struct page_t *page = (struct page_t *)heap.start;
+		 page_table[i][j] = (struct page_t *)heap.start;
+		 heap.start = (void *)ROUNDUP(heap.start + PAGE_SIZE, PAGE_SIZE);
+	     page -> lock = &lock[i];
+		 page -> belong = cnt++;
+		 page -> block_size = DataSize[j];
+		 page -> next = NULL;
+		 for (uintptr_t k = (uintptr_t)heap.start + 128; 
+		     k != (uintptr_t)heap.start +PAGE_SIZE;
+		     k += DataSize[j]) {
+			 ptr[page -> belong][sum[page ->belong]++] = k;	
+		 }
+	  }
+  }
 }
 
 MODULE_DEF(pmm) = {
