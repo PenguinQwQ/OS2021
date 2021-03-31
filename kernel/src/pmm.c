c5fe1e0c9cfc057f252e8e2e42e2fb41d7fd3088
tracer-nju <tracer@nju.edu.cn>
2021-03-31 08:16:23 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: e0386af4c1124b03894b65904ec0a97c     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  08:16:23 up 21:20,  3 users,  load average: 1.30, 0.83, 0.46

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -13,7 +13,7 @@ typedef struct{
 
 
 static int DataSize[MAX_DATA_SIZE] = {8, 16, 32, 64, 128, 512, 1024, 2048};
-static int power[MAX_DATA_SIZE]    = {7, 15, 31, 63, 128, 15, 10, 5};
+static int power[MAX_DATA_SIZE]    = {7, 15, 31, 63, 128, 15, 5, 5};
 
 struct page_t{
 	spinlock_t *lock;
