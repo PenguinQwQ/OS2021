c7d319d8442c6a23bb73117003e20678a48b2bd5
tracer-nju <tracer@nju.edu.cn>
2021-03-31 13:37:29 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: e0386af4c1124b03894b65904ec0a97c     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  13:37:29 up 1 day, 15 min,  4 users,  load average: 0.62, 0.61, 0.41

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -22,7 +22,7 @@ void test1() {
 		}
 	    else {
 			pmm->free(st[top - 1]);
-			printf("111\n");
+			printf("%d\n", st2[top - 1]);
 			assert(st[top - 1] == pmm->alloc(st2[top - 1]));
 			printf("111\n");
 			pmm->free(st[top - 1]);
