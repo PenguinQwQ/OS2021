46044659cb7dffe6ac61f5b3e4852a3977dd6074
tracer-nju <tracer@nju.edu.cn>
2021-03-31 13:30:36 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: e0386af4c1124b03894b65904ec0a97c     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  13:30:36 up 1 day, 8 min,  3 users,  load average: 0.29, 0.40, 0.27

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -184,7 +184,7 @@ void deal_Slow_free(uintptr_t left) {
 			if (List -> val_r[now] <= left) {
 				nxt = List -> val_next[now];
 				assert(nxt);
-				printf("%p %p\n", List -> val_l[nxt], right);
+				printf("%p %p %p\n", List -> val_l[nxt], List -> val_r[nxt], right);
 				assert(List -> val_l[nxt] >= right);
 				break;	
 			}
