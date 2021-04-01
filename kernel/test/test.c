33f9399a84abd9c472a3e0739fb745c48630fd83
tracer-nju <tracer@nju.edu.cn>
2021-04-01 03:26:25 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: e0386af4c1124b03894b65904ec0a97c     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  03:26:25 up 1 day,  3:06,  5 users,  load average: 0.57, 0.28, 0.26

--- kernel/test/test.c
+++ kernel/test/test.c
@@ -5,7 +5,13 @@ int cpu_count() {
 	return smp;	
 }
 
+void entry() {
+	printf("%d\n", cpu_current());	
+} 
+
 int main() {
-	pmm -> init();	
+	pmm -> init();
+	for (int i = 0; i < smp; i++)
+		create(entry);	
 	return 0;
 }
