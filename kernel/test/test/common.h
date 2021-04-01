0348b2ab06abc956182187f1f8f7b783525e0eb9
tracer-nju <tracer@nju.edu.cn>
2021-04-01 07:00:55 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: e0386af4c1124b03894b65904ec0a97c     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  07:00:55 up 1 day,  4:26,  3 users,  load average: 0.26, 0.23, 0.19

---
+++ kernel/test/test/common.h
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <kernel.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+#define ROUNDUP(a, sz) ((((uintptr_t)a) + (sz) - 1) & ~((sz)- 1))
+
+int cpu_count();
+
+int cpu_current();
+
+int cpu_count();
---
+++ kernel/test/test/test.c
@@ -0,0 +1,41 @@
+#include <common.h>
+#include <threads.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+#define smp 8
+
+static int ttid[smp], sum = 0;
+
+int cpu_current() {
+	for (int i = 0; i < smp; i++) 
+		if (syscall(SYS_gettid) == ttid[i]) return i;	
+	assert(0);
+}
+
+int cpu_count() {
+	return smp;	
+}
+
+void task1() { // smoke task
+	for (int i = 0; i < 100000; i++) {
+		int p = rand() % 10;
+		if (p <= 4) pmm->alloc(rand() % 1024);	
+		else if (p <= 7) pmm -> alloc(4096);
+		else pmm->alloc(rand());
+	}	
+}
+
+void entry(int tid) {
+	lock();
+	ttid[sum++] = syscall(SYS_gettid);
+	unlock();
+	task1();
+	printf("task_1 success on %d!\n", cpu_current());
+} 
+
+int main(int argc, char *argv[]) {
+	pmm -> init();
+	for (int i = 0; i < smp; i++)
+		create(entry);	
+	return 0;
+}
