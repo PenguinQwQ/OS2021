d59d61ba2e61e8f7356e6179aaa46241f376e0c9
tracer-nju <tracer@nju.edu.cn>
2021-04-01 06:36:42 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: e0386af4c1124b03894b65904ec0a97c     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  06:36:42 up 1 day,  4:02,  5 users,  load average: 0.75, 0.45, 0.21

--- kernel/test/test.c
+++ kernel/test/test.c
@@ -7,12 +7,8 @@
 static int ttid[smp], sum = 0;
 
 int cpu_current() {
-	lock();
 	for (int i = 0; i < smp; i++) 
-		if (syscall(SYS_gettid) == ttid[i]) {
-			unlock();
-			return i;	
-		}
+		if (syscall(SYS_gettid) == ttid[i]) return i;	
 	assert(0);
 }
 
@@ -22,7 +18,7 @@ int cpu_count() {
 
 void entry(int tid) {
 	lock();
-	ttid[sum++] = cpu_current();
+	ttid[sum++] = syscall(SYS_gettid);
 	unlock();
 	printf("%d\n", sum);	
 } 
