3563d4ecb82a940898c131a0394c71afe451836f
tracer-nju <tracer@nju.edu.cn>
2021-04-01 11:28:36 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: e0386af4c1124b03894b65904ec0a97c     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  11:28:36 up 1 day,  8:11,  4 users,  load average: 1.35, 2.56, 2.02

--- kernel/test/test.c
+++ kernel/test/test.c
@@ -62,7 +62,7 @@ void finish() {
 
 void task1() { // smoke task
 	for (int i = 0; i < MAXN; i++) {
-		int p = rand() % 15, sz, bj = 0;
+		int p = rand() % 15, sz, bj = 0; p = 9;
 		if (p <= 5)      sz = rand() % 128 + 1;
 		else if (p <= 8) sz = 4096;
 		else if (p <= 9) sz = (rand() & ((16 << 20) - 1)) + 1;
