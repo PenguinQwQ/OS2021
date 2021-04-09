32cb1b5bda3902c621de294a034690318df95b57
tracer-nju <tracer@nju.edu.cn>
2021-04-09 15:25:47 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: 7f4629b0ec5b41de8dd945d4df183525     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  15:25:47 up  6:34,  4 users,  load average: 0.04, 0.16, 0.17

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -157,6 +157,7 @@ void *SlowSlab_path(int id, size_t sz) {
 	if (sz <= 4096 && 
 		BigSlab_Size[id] > 0) return (void *)BigSlab[id][--BigSlab_Size[id]];
 	else {
+		sz = pmax(sz, 4096);
 		spinlock(&BigLock_Slow);
 		void *tep = Slow_path(sz);
 		spinunlock(&BigLock_Slow);
@@ -166,7 +167,6 @@ void *SlowSlab_path(int id, size_t sz) {
 
 void* deal_slab(int id, int kd, size_t sz) {
 	if (kd == MAX_DATA_SIZE) {
-		sz = pmax(sz, 4096);
 		return SlowSlab_path(id, sz);
 		spinlock(&BigLock_Slow);
 		void *tep = Slow_path(sz);
