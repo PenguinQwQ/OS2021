29475a1128df9bed3815ad752053113d5580fe41
tracer-nju <tracer@nju.edu.cn>
2021-04-09 12:54:33 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: 7f4629b0ec5b41de8dd945d4df183525     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  12:54:33 up  4:03,  4 users,  load average: 0.35, 0.18, 0.15

--- kernel/test/test.c
+++ kernel/test/test.c
@@ -2,8 +2,8 @@
 #include <threads.h>
 #include <unistd.h>
 #include <sys/syscall.h>
-#define smp  1
-#define MAXN 100
+#define smp  8
+#define MAXN 10000
 
 static int ttid[smp], sum = 0;
 
@@ -62,7 +62,7 @@ void finish() {
 
 void task1() { // smoke task
 	for (int i = 0; i < MAXN; i++) {
-		int p = rand() % 11, sz, bj = 0;
+		int p = rand() % 15, sz, bj = 0;
 		if (p <= 6)      sz = rand() % 128 + 1;
 		else if (p <= 9) sz = 4096;
 		else if (p <= 10) sz = (rand() & ((16 << 20) - 1)) + 1;
