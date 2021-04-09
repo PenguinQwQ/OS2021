a0e006ccb5099835fed85ba9b6892ad50d3ab07a
tracer-nju <tracer@nju.edu.cn>
2021-04-09 13:14:25 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: 7f4629b0ec5b41de8dd945d4df183525     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  13:14:25 up  4:23,  4 users,  load average: 0.13, 0.14, 0.11

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -272,10 +272,8 @@ void debug_count() {
 //	printf("sum1:%d sum1: %d\n", sup, sub);
 }
 
-spinlock_t a;
 
 static void *kalloc(size_t size) {  
-  spinlock(&a);
   assert(size);
   if ((size >> (size_t)20) >= (size_t)16) return NULL;
   
@@ -287,21 +285,18 @@ static void *kalloc(size_t size) {
 	spinlock(&lock[id]);
 	space = deal_slab(id, kd, size);
 	spinunlock(&lock[id]);	  
-  spinunlock(&a);
 	return space;
   }
   else if(kd == MAX_DATA_SIZE) {
 	spinlock(&BigLock_Slab);
 	space = SlowSlab_path();
 	spinunlock(&BigLock_Slab);
-  spinunlock(&a);
 	return space;
   }
   else if (kd == MAX_DATA_SIZE + 1) {
 	spinlock(&BigLock_Slow);
 	space = Slow_path(size);
 	spinunlock(&BigLock_Slow);
-  spinunlock(&a);
 	return space;  
   }
   else assert(0);
@@ -316,7 +311,6 @@ int judge_free(void *ptr) {
 }
 
 static void kfree(void *ptr) {
-  spinlock(&a);
   int kd = judge_free(ptr);
   if (kd == 1) {  
 	struct page_t *now = (struct page_t *) ((uintptr_t)ptr & (~(PAGE_SIZE - 1)));
@@ -335,7 +329,6 @@ static void kfree(void *ptr) {
 	  spinunlock(&BigLock_Slow);
   }
   else assert(0);
-  spinunlock(&a);
 }
 
 
