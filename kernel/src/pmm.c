f087b061ac5e24afd7f32fe5dc7ac0f6e8c01232
tracer-nju <tracer@nju.edu.cn>
2021-04-09 23:47:22 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: 7f4629b0ec5b41de8dd945d4df183525     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  23:47:22 up  8:02,  4 users,  load average: 0.08, 0.20, 0.18

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -236,7 +236,6 @@ void* deal_slab(int id, int kd, size_t sz) {
 	}
 	else {
 		now = page_table[id][kd];
-  spinlock(&lock_all);  
 		while (now != NULL && now -> remain == 0) now = now ->next;
   spinunlock(&lock_all);  
 		assert(now != NULL);
