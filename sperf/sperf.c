27d8f1d50bd962cf2d62bcfe58b33c5496c4c87f
tracer-nju <tracer@nju.edu.cn>
2021-04-22 22:52:49 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: 56c9e7fe56e6497d8df534fda14c807a     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  22:52:50 up  7:26,  2 users,  load average: 0.07, 0.16, 0.11

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -3,12 +3,14 @@
 #include <unistd.h>
 const int N = 65536;
 
-char *exec_argv[N] = { "strace", "-T"};
+char *string_name[] = {"strace", "-T"};
+char *exec_argv[N];
 char *exec_envp[]  = { "PATH=/bin", NULL};
 
 int main(int argc, char *argv[]) {
-/*  for (int i = 1; i < argc; i++) exec_argv[i] = argv[i];
-  exec_argv[argc] = NULL;*/
+  for (int i = 0; i < 2; i++) exec_argv[i] = string_name[i];
+  for (int i = 1; i < argc; i++) exec_argv[i + 1] = argv[i];
+  exec_argv[argc + 1] = NULL;
   execve("strace",          exec_argv, exec_envp);
   execve("/bin/strace",     exec_argv, exec_envp);
   execve("/usr/bin/strace", exec_argv, exec_envp);
