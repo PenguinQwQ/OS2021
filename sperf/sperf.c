8ce4ff046bbe03cec1d29318dda4b44d64d11a9d
tracer-nju <tracer@nju.edu.cn>
2021-04-22 22:31:37 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: 56c9e7fe56e6497d8df534fda14c807a     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  22:31:37 up  7:05,  2 users,  load average: 0.33, 0.12, 0.04

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -3,12 +3,11 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-  char *exec_argv[] = { "strace", "ls", NULL, };
+  char *exec_argv[] = { "strace", "ls", ,"-l", NULL, };
   char *exec_envp[] = { "PATH=/bin", NULL, };
   execve("strace",          exec_argv, exec_envp);
   execve("/bin/strace",     exec_argv, exec_envp);
   execve("/usr/bin/strace", exec_argv, exec_envp);
-  printf("3232\n");
   perror(argv[0]);
   exit(EXIT_FAILURE);
 }
