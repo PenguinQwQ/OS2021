60e4eeb48919d49f065a91ccbddde27667cb68e8
tracer-nju <tracer@nju.edu.cn>
2021-04-26 14:38:29 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: 56c9e7fe56e6497d8df534fda14c807a     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  14:38:29 up 22:14,  4 users,  load average: 0.32, 0.11, 0.04

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[]) {
   for (int i = 1; i < argc; i++) exec_argv[i + 1] = argv[i];
   exec_argv[argc + 1] = NULL;
   int fd[2];
-  if (pipe(fd) != 0) assert(0);
+  if (pipe2(fd, O_NONBLOCK) != 0) assert(0);
   dup2(fd[1], 2);
   int pid = fork();
   if (pid == 0) {
@@ -27,8 +27,8 @@ int main(int argc, char *argv[]) {
   }
   else {
 	close(fd[1]);
-	while(waitpid(pid, NULL, WNOHANG) == 0 && read(fd[0], buf, 1) > 0) {
-		int cnt = read(fd[0], buf, 1);
+	while(waitpid(pid, NULL, WNOHANG) == 0) {
+		int cnt = read(fd[0], buf, sizeof(buf));
 		if (cnt >= 0) buf[cnt] = 0;
 		if (cnt > 0) printf("%s", buf);
 	}
