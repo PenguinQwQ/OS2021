d296e3971bb5155c61931330fb95cb8771352d4e
tracer-nju <tracer@nju.edu.cn>
2021-04-26 13:25:40 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: 56c9e7fe56e6497d8df534fda14c807a     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  13:25:40 up 21:01,  3 users,  load average: 0.12, 0.14, 0.10

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -19,7 +19,6 @@ int main(int argc, char *argv[]) {
   int pid = fork();
   if (pid == 0) {
 	close(fd[0]);
-	return 0;
 	execve("strace",          exec_argv, exec_envp);
 	execve("/bin/strace",     exec_argv, exec_envp);
 	execve("/usr/bin/strace", exec_argv, exec_envp);
@@ -29,7 +28,6 @@ int main(int argc, char *argv[]) {
   else {
 	close(fd[1]);
 	wait(NULL);
-	return 0;
 	while(waitpid(pid, NULL, WNOHANG) == 0) {
 		int cnt = read(fd[0], buf, sizeof(buf));
 		buf[cnt] = 0;
