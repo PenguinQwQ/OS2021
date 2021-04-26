9455dfd487f2151a33831106da5b1fd016c468a8
tracer-nju <tracer@nju.edu.cn>
2021-04-26 13:36:16 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: 56c9e7fe56e6497d8df534fda14c807a     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  13:36:16 up 21:11,  3 users,  load average: 0.00, 0.02, 0.06

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -27,7 +27,7 @@ int main(int argc, char *argv[]) {
   }
   else {
 	close(fd[1]);
-	while(waitpid(pid, NULL, WNOHANG) == 0) {
+	while(waitpid(pid, NULL, WNOHANG) <= 0) {
 		int cnt = read(fd[0], buf, sizeof(buf));
 		buf[cnt] = 0;
 		if (cnt > 0) printf("%s", buf);
