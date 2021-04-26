943e66834ea1778a1d7ed1fbc9c3505936d0697b
tracer-nju <tracer@nju.edu.cn>
2021-04-26 12:23:22 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: 56c9e7fe56e6497d8df534fda14c807a     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  12:23:22 up 19:59,  3 users,  load average: 0.04, 0.05, 0.01

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -6,14 +6,16 @@
 
 char *exec_argv[N] = {"strace", "-T"};
 char *exec_envp[]  = { "PATH=/bin", NULL};
-int fd[2];
 
 int main(int argc, char *argv[]) {
   for (int i = 1; i < argc; i++) exec_argv[i + 1] = argv[i];
   exec_argv[argc + 1] = NULL;
+  int fd[2];
   if (pipe(fd) != 0) assert(0);
+  dup2(fd[1], 2);
   int pid = fork();
   if (pid == 0) {
+	close(fd[1]);
 	execve("strace",          exec_argv, exec_envp);
 	execve("/bin/strace",     exec_argv, exec_envp);
 	execve("/usr/bin/strace", exec_argv, exec_envp);
@@ -21,9 +23,10 @@ int main(int argc, char *argv[]) {
 	exit(EXIT_FAILURE);
   }
   else {
+	close(fd[0]);
 	char buf[10];
-	int cnt = read(fd[0], buf, sizeof(buf));
-	printf("%d\n", cnt); 
+	int cnt = read(fd[1], buf, sizeof(buf));
+	printf("%s\n", buf); 
 	return 0;	  
   }
 }
