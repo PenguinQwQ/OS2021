927ab7fa6d83661fcef96d8804c0087883f134d7
tracer-nju <tracer@nju.edu.cn>
2021-04-27 04:15:39 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: 56c9e7fe56e6497d8df534fda14c807a     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  04:15:39 up 22:49,  4 users,  load average: 0.05, 0.11, 0.09

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -38,7 +38,7 @@ int main(int argc, char *argv[]) {
 	exec_argv[3] = tep_argv;
 	for (int i = 1; i < argc; i++) exec_argv[i + 3] = argv[i];
 	int now = 0;
-	while(exec_argv[now != NULL])
+	while(exec_argv[now] != NULL)
 		printf("%s\n". exec_argv[now]), now++;
 	exec_argv[argc + 1] = NULL;
 	execve("strace",          exec_argv, exec_envp);
