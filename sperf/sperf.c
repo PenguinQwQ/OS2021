beba3825454594aae29813f52059ede64a35da04
tracer-nju <tracer@nju.edu.cn>
2021-04-27 04:25:05 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: 56c9e7fe56e6497d8df534fda14c807a     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  04:25:05 up 22:59,  4 users,  load average: 0.31, 0.33, 0.19

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -34,7 +34,7 @@ int main(int argc, char *argv[]) {
 
 	char tep_argv[100];
 	int id = getpid();
-    sprintf(tep_argv, "/proc/%d/fd/%d", id, fd[1]);
+    sprintf(tep_argv, "/proc/%d/fd/%d", id, 2);
 	exec_argv[3] = tep_argv;
 	for (int i = 1; i < argc; i++) exec_argv[i + 3] = argv[i];
 	int now = 0;
