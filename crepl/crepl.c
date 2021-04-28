40c45a312e05e33fd64c91dc853ab685a86e34b6
tracer-nju <tracer@nju.edu.cn>
2021-04-28 02:15:59 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: 56c9e7fe56e6497d8df534fda14c807a     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  02:15:59 up 1 day,  5:43,  4 users,  load average: 0.01, 0.06, 0.07

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -4,19 +4,34 @@
 #include <assert.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <dlopen.h>
 
-static char name[16];
+static char cname[16];
+static char sname[16];
 static char line[4096];
 
 void makedoc() {
     char filename_template[] = "XXXXXX";
 	int fd = mkstemp(filename_template);
 	assert(fd > 0);
-	strcpy(name, filename_template);
-	name[6] = '.', name[7] = 'c', name[8] = '\0';
+	strcpy(cname, filename_template);
+	cname[6] = '.', cname[7] = 'c', cname[8] = '\0';
 	rename(filename_template, name);
+	strcpy(sname, filename_template);
+	sname[6] = '.', sname[7] = 's', sname[8] = 'o', sname[9] = '\0';
 	write(fd, line, strlen(line));
 	close(fd);
+}
+
+char *exec_argv[16] = {"gcc", "-fPIC", "-shared"};
+
+void compile() {
+	exec_argv[3] = cname;
+	exec_argv[4] = "-o";
+	exec_argv[5] = sname;
+	int pid = fork();
+	if (pid == 0) execvp("gcc", exec_argv);
+	else wait(NULL);	
 }
 
 int main(int argc, char *argv[]) {
@@ -27,7 +42,7 @@ int main(int argc, char *argv[]) {
       break;
     }
 	makedoc();
-
+	compile();
   }
   return 0;
 }
