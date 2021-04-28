5c143721e4ef900aa4d9784b9e3675f2059ca0fd
tracer-nju <tracer@nju.edu.cn>
2021-04-28 05:04:53 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: 56c9e7fe56e6497d8df534fda14c807a     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  05:04:53 up 1 day,  7:21,  4 users,  load average: 0.06, 0.11, 0.09

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -50,9 +50,11 @@ void makedoc() {
 	close(fd);
 }
 
-char *exec_argv[16] = {"gcc", "-fPIC", "-shared", "-m32"};
+char *exec_argv[16] = {"gcc", "-fPIC", "-shared"};
 
 bool compile() {
+	if (sizeof(int *) == 4) sprintf(exec_argv[3], "-m%d", 32);
+	else sprintf(exec_argv[3], "-m%d", 64);
 	int p = 4;
 	exec_argv[p] = cname;
 	exec_argv[p + 1] = "-o";
