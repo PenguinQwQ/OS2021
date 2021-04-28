fde099c2d7dd2275b4359c39ea3a6c0049d5fb63
tracer-nju <tracer@nju.edu.cn>
2021-04-28 01:39:51 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: 56c9e7fe56e6497d8df534fda14c807a     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  01:39:52 up 1 day,  5:15,  4 users,  load average: 0.10, 0.11, 0.04

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -3,17 +3,23 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <unistd.h>
+#include <fcntl.h>
+
 int main(int argc, char *argv[]) {
   static char line[4096];
+  static char name[16];
   while (1) {
     printf("crepl> ");
     fflush(stdout);
     if (!fgets(line, sizeof(line), stdin)) {
       break;
     }
-    char filename_template[] = "XXXXXX.c";
+    char filename_template[] = "XXXXXX";
 	int fd = mkstemp(filename_template);
 	assert(fd > 0);
+	strcpy(name, filename_template);
+	name[6] = '.', name[7] = 'c', name[8] = '\0';
+	rename(filename_template, name);
 	write(fd, line, strlen(line));
 	close(fd);
   }
