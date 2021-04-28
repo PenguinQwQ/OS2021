4d35f0778c515c7bfecd790af394cd0c898c2dbf
tracer-nju <tracer@nju.edu.cn>
2021-04-28 01:26:30 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: 56c9e7fe56e6497d8df534fda14c807a     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  01:26:30 up 1 day,  5:02,  3 users,  load average: 0.00, 0.02, 0.00

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
+#include <assert.h>
+#include <unistd.h>
 int main(int argc, char *argv[]) {
   static char line[4096];
   while (1) {
