b2a7462af835068a44966555d8e121116eb5dd97
tracer-nju <tracer@nju.edu.cn>
2021-05-02 03:19:08 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: 8462a21a49bd47f386f1b392f1b8981e     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  03:19:08 up  1:10,  7 users,  load average: 0.33, 0.22, 0.31

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -89,8 +89,8 @@ int main(int argc, char *argv[], char *envp[]) {
   int pid = fork();
   if (pid == 0) {
 	int file = open("/dev/null", 0);
-	assert(file > 0);
 	if (sizeof(int *) == 4)while(1);
+	assert(file > 0);
 	dup2(file, 1);
 	dup2(file, 2);
 	close(fd[0]);
