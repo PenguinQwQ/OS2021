f2b173bce685b0a273fa9650dbc3d6fe596b90ee
tracer-nju <tracer@nju.edu.cn>
2021-05-02 14:48:35 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: 8462a21a49bd47f386f1b392f1b8981e     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  14:48:35 up  2:42,  6 users,  load average: 0.02, 0.15, 0.20

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -90,8 +90,8 @@ int main(int argc, char *argv[], char *envp[]) {
   if (pid == 0) {
 	int file = open("/dev/null", 0);
 	assert(file > 0);
-//	dup2(file, 1);
-//	dup2(file, 2);
+	dup2(file, 1);
+	dup2(file, 2);
 	close(fd[0]);
 	char tep_argv[100];
 	int id = getpid();
@@ -120,6 +120,7 @@ int main(int argc, char *argv[], char *envp[]) {
 			||  envp[i][j] == '\0') {
 				tmp[current] = '\0';
 				strcat(tmp, tmp2);
+				exec_argv[0] = tmp;
 				execve(tmp, exec_argv, envp);
 				current = 0;
 			}
