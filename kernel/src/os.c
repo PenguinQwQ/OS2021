042ad5ec50dd17e28e0d115fe063f363b9050e5e
tracer-nju <tracer@nju.edu.cn>
2021-05-05 08:53:41 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: 8462a21a49bd47f386f1b392f1b8981e     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  08:53:41 up 15:32,  5 users,  load average: 1.38, 0.83, 0.60

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -6,6 +6,7 @@ spinlock_t trap_lock;
 void func(void *args) {
 	int ti = 0;
 	while(1) {
+		assert(ienabled() == true);
 		kmt -> spin_lock(&trap_lock);
 		printf("Hello from CPU#%d for %d times with arg %s!\n", cpu_current(), ti++, args);	  
 		kmt -> spin_unlock(&trap_lock);
