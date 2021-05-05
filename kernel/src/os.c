9a2406905aafea09907d5b11c06a497c7622c56c
tracer-nju <tracer@nju.edu.cn>
2021-05-05 09:26:00 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: 8462a21a49bd47f386f1b392f1b8981e     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  09:26:00 up 16:04,  5 users,  load average: 0.75, 0.58, 0.59

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -19,11 +19,11 @@ static void os_init() {
   kmt->spin_init(&trap_lock, "os_trap");
   kmt->create(pmm -> alloc(sizeof(task_t)), "hello", func, "aa");
   kmt->create(pmm -> alloc(sizeof(task_t)), "hello", func, "bb");
-  kmt->create(pmm -> alloc(sizeof(task_t)), "hello", func, "cc");
-  kmt->create(pmm -> alloc(sizeof(task_t)), "hello", func, "dd");
-  kmt->create(pmm -> alloc(sizeof(task_t)), "hello", func, "ee");
-  kmt->create(pmm -> alloc(sizeof(task_t)), "hello", func, "ff");
-  kmt->create(pmm -> alloc(sizeof(task_t)), "hello", func, "gg");
+//  kmt->create(pmm -> alloc(sizeof(task_t)), "hello", func, "cc");
+//  kmt->create(pmm -> alloc(sizeof(task_t)), "hello", func, "dd");
+//  kmt->create(pmm -> alloc(sizeof(task_t)), "hello", func, "ee");
+//  kmt->create(pmm -> alloc(sizeof(task_t)), "hello", func, "ff");
+//  kmt->create(pmm -> alloc(sizeof(task_t)), "hello", func, "gg");
 //  kmt->create(pmm -> alloc(sizeof(task_t)), "hello", func, "hh");
 }
 
