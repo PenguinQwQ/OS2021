49fe61a0fb6cd423c0ce03b68f141a79a503f32c
tracer-nju <tracer@nju.edu.cn>
2021-05-06 14:06:02 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: 8462a21a49bd47f386f1b392f1b8981e     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  14:06:02 up 21:12,  6 users,  load average: 0.15, 0.28, 0.30

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -2,17 +2,17 @@
 #define MAX_CPU 128
 
 spinlock_t trap_lock;
-/*
+
 void func(void *args) {
 	int ti = 0;
 	while(1) {
 		printf("Hello from CPU#%d for %d times with arg %s!\n", cpu_current(), ti++, args);	  
 	}
 }
-*/
+
 
 int Lists_sum = 0;
-
+/*
 sem_t empty, fill;
 
 void producer() {
@@ -22,13 +22,15 @@ void producer() {
 void comsumer() {
 	while(1){kmt->sem_wait(&fill); putch(')'); kmt->sem_signal(&empty);}
 }
-
+*/
 static void os_init() {
   Lists_sum = 0;
   pmm->init();
   kmt->init();
   kmt->spin_init(&trap_lock, "os_trap");
-
+  kmt -> create(pmm -> alloc(sizeof(task_t)), "hello", func, "aa");
+  kmt -> create(pmm -> alloc(sizeof(task_t)), "hello", func, "bb");
+/*
   kmt -> sem_init(&empty, "empty", 5);
   kmt -> sem_init(&fill,  "fill" , 0);
 //  for (int i = 0; i < 4; i++) 
@@ -36,6 +38,7 @@ static void os_init() {
 	
   //for (int i = 0; i < 5; i++) 
 	  kmt->create(pmm->alloc(sizeof(task_t)), "consumer", comsumer, NULL);
+	  */
 }
 
 static void os_run() {
