be625ece7b4eb662ac551bbfd06dc172dcfeb6bb
tracer-nju <tracer@nju.edu.cn>
2021-05-07 11:04:26 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: 8462a21a49bd47f386f1b392f1b8981e     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  11:04:26 up 1 day,  2:46,  6 users,  load average: 0.17, 0.51, 0.33

--- kernel/src/thread.c
+++ kernel/src/thread.c
@@ -59,7 +59,6 @@ int status[MAX_CPU];
 static void spin_lock(spinlock_t *lk) {
 	int i = ienabled();
 	iset(false);
-	assert(lk -> cpu_id != cpu_current() );
 	while(atomic_xchg(&lk -> lock, 1));	
 	int id = cpu_current();
 	if (cnt[id] == 0) status[id] = i;
