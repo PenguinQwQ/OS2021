ffc3f74777e932141a4781edb338636e3540cc33
tracer-nju <tracer@nju.edu.cn>
2021-05-07 12:21:31 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: 8462a21a49bd47f386f1b392f1b8981e     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  12:21:31 up 1 day,  4:03,  6 users,  load average: 1.01, 0.60, 0.50

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -33,7 +33,7 @@ static void os_init() {
   kmt->init();
   kmt->spin_init(&trap_lock, "os_trap");
 //  dev -> init();
-
+/*
   kmt -> create(pmm -> alloc(sizeof(task_t)), "hello", func, "aa");
   kmt -> create(pmm -> alloc(sizeof(task_t)), "hello", func, "bb");
   kmt -> create(pmm -> alloc(sizeof(task_t)), "hello", func, "cc");
@@ -44,7 +44,7 @@ static void os_init() {
   kmt -> create(pmm -> alloc(sizeof(task_t)), "hello", func, "hh");
   kmt -> create(pmm -> alloc(sizeof(task_t)), "hello", func, "ii");
   kmt -> create(pmm -> alloc(sizeof(task_t)), "hello", func, "jj");
-/*
+*/
   kmt -> sem_init(&empty, "empty", 1);
   kmt -> sem_init(&fill,  "fill" , 0);
  //for (int i = 0; i < 4; i++) 
@@ -52,7 +52,7 @@ static void os_init() {
 	
   //for (int i = 0; i < 5; i++) 
 	  kmt->create(pmm->alloc(sizeof(task_t)), "consumer", comsumer, NULL);
-  */
+  
 }
 
 static void os_run() {
