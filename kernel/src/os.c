f90941abb68e258c97d73c5ba9ce9e71d8b374a4
tracer-nju <tracer@nju.edu.cn>
2021-05-07 08:27:20 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: 8462a21a49bd47f386f1b392f1b8981e     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  08:27:20 up 1 day,  1:24,  7 users,  load average: 0.47, 0.28, 0.22

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -74,7 +74,7 @@ static Context* os_trap(Event ev, Context *context) {
 		origin[id].ctx = context;
 		current[id] -> status = RUNNING;
 	}
-	assert(current[id] -> status != BLOCKED);
+	assert(current[id] -> status != SUITABLE);
 
 	for (int i = 0; i < Lists_sum; i++)
 		if (ev.event == Lists[i].event || Lists[i].event == EVENT_NULL)
