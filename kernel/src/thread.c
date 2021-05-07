415f8e357c37f16f09845dc5ac4c941c5da15100
tracer-nju <tracer@nju.edu.cn>
2021-05-07 07:56:27 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: 8462a21a49bd47f386f1b392f1b8981e     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  07:56:27 up 1 day, 53 min,  6 users,  load average: 0.14, 0.19, 0.34

--- kernel/src/thread.c
+++ kernel/src/thread.c
@@ -105,6 +105,7 @@ static void sem_wait(sem_t *sem) {
 		sem -> head -> next = tep;
 		kmt->spin_unlock(&sem -> lock);
 		yield();
+		assert(flag == 1);
 		assert(cpu_current() == id);
 		assert(current[id] -> status == RUNNING);
 	}
