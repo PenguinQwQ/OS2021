30ff63d2f804b8e7b259f40c21dbe58824034618
tracer-nju <tracer@nju.edu.cn>
2021-05-09 10:00:26 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: f32005bd320646fa857afeab28915d22     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  10:00:26 up 22 min,  4 users,  load average: 2.08, 1.65, 1.58

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -46,7 +46,7 @@ static void os_init() {
   kmt -> create(pmm -> alloc(sizeof(task_t)), "hello", func, "ii");
   kmt -> create(pmm -> alloc(sizeof(task_t)), "hello", func, "jj");
 
-*/ 
+*//* 
   kmt -> sem_init(&empty, "empty", 10);
   kmt -> sem_init(&fill,  "fill" , 0);
  for (int i = 0; i < 4; i++) 
@@ -54,7 +54,7 @@ static void os_init() {
 	
  for (int i = 0; i < 5; i++) 
 	  kmt->create(pmm->alloc(sizeof(task_t)), "consumer", comsumer, NULL);
-
+*/
 }
 
 static void os_run() {
