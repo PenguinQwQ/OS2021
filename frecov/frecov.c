49446641e59cacae7a04bdc344f0fb544e251062
tracer-nju <tracer@nju.edu.cn>
2021-06-01 11:27:41 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: 9070006d94c14583a99285cfba1b93d6     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  11:27:42 up 14:18,  5 users,  load average: 0.26, 0.26, 0.24

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -1,2 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+struct fat_header{
+	uint8_t BS_jmpBoot[3];
+	uint8_t BS_OEMName[8];
+	uint8_t BPB_BytsPerSer[2];
+	uint8_t BPB_SecPerClus;
+	uint8_t BPB_RsvdSecCnt[2];
+	uint8_t BPB_NumFATs;
+	uint8_t BPB_RootEntCnt[2];
+	uint8_t BPB_TotSec16[2];
+	uint8_t BPB_Media;
+	uint8_t BPB_FATSz16[2];
+	uint8_t BPB_SecPerTr[2];
+	uint8_t BPB_NumHead[2];
+	uint8_t BPB_HiddSec[4];
+	uint8_t BPB_TotSec32[4];
+	uint8_t BS_DrvNum;
+	uint8_t BS_Reserved;
+	uint8_t BS_BootSig;
+	uint8_t BS_VolID;
+	uint8_t BS_VolLab[11];
+	uint8_t BS_FilSysType[8];
+	uint8_t BS_empty[448];
+	uint8_t Signature_word[2];	
+};
+
 int main(int argc, char *argv[]) {
+	panic_on(sizeof(struct fat_header) != 512, "bad header!");
+
 }
