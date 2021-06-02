83a958737cb732bfc2541fe8f04caba26c6d0f7c
tracer-nju <tracer@nju.edu.cn>
2021-06-02 08:41:34 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: 9070006d94c14583a99285cfba1b93d6     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  08:41:35 up 21:54,  6 users,  load average: 0.22, 0.22, 0.19

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -150,6 +150,7 @@ void SolveLongName(struct long_file * now) {
 int find_info(struct short_file * now) {
 	uint32_t loc = now -> FstClusHl;
 	loc = (loc << 16) | now -> FstClusLO;
+	printf("%x\n", loc);
 	if (loc == 0) return 0;
 	return 1;	
 }
