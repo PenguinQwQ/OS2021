5c8519441cfd937d72b377801512a1abe8afbb36
tracer-nju <tracer@nju.edu.cn>
2021-06-02 01:05:44 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: 9070006d94c14583a99285cfba1b93d6     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  01:05:44 up 17:33,  5 users,  load average: 0.00, 0.03, 0.07

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -50,7 +50,8 @@ int main(int argc, char *argv[]) {
 	uint32_t fat1, fat2, root_dir;
 	fat1 = (uint32_t)disk -> BPB_RsvdSecCnt * 512;
 	fat2 = fat1 + disk -> BPB_FATSz32 * 512;
-	root_dir = fat2 + disk -> BPB_FATSz32 * 512 + disk -> BPB_RootClus * 512;
+	root_dir = fat1 + disk -> NUMFATs * disk -> BPB_FATSz32 * 512 + \
+	           disk -> BPB_RootClus * 512;
 
 	printf("%x %x %x %x\n", fat1, fat2, root_dir, disk -> BPB_RootClus);
 
