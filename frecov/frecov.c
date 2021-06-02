048a8bad888cb083150454c0d61fcfb24b5d49ad
tracer-nju <tracer@nju.edu.cn>
2021-06-02 07:31:58 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: 9070006d94c14583a99285cfba1b93d6     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  07:31:58 up 20:44,  5 users,  load average: 0.25, 0.18, 0.16

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -133,7 +133,7 @@ void get_name(char c) {
 
 void deal() {
 	for (int i = 0; i < tot[1]; i++) {
-		struct short_file *tep = (struct short_file *)divided[1][i];
+		struct short_file *tep = (struct short_file *)(void *)divided[1][i];
 
 		while ((uint32_t)tep < divided[1][i] + disk -> BPB_SecPerClus * 512) {
 			if (tep -> DIR_Name[8] != 'B' && tep -> DIR_Name[9] != 'M' && \
