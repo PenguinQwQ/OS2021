7bd7dc072535a2ea2550e4d409fe8008ad2961bf
tracer-nju <tracer@nju.edu.cn>
2021-06-02 03:48:26 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: 9070006d94c14583a99285cfba1b93d6     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  03:48:26 up 20:11,  5 users,  load average: 0.33, 0.30, 0.22

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -116,7 +116,10 @@ void divide() {
 		if (id == -1) id = judge_dir(loc);
 		if (id == -1) id = judge_bmphead(loc);
 		if (id == -1) id = 3, divided[3][tot[3]++] = loc;
+		
+		#ifdef DEBUG
 		printf("loc=0x%x id=%d\n", loc, id);
+		#endif
 	}
 
 }
