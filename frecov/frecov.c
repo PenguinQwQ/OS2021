19b6d90a9f6da90dd8c792dc8631c2c7b8f370dc
tracer-nju <tracer@nju.edu.cn>
2021-06-02 12:39:44 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: 9070006d94c14583a99285cfba1b93d6     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  12:39:44 up 1 day,  1:17,  6 users,  load average: 1.13, 0.88, 0.83

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -235,7 +235,6 @@ int find_info(struct short_file * now) {
 		if (off == disk -> BPB_BytsPerSer * disk -> BPB_SecPerClus) {
 			start = findClus(now_loc, cnt, id), off = 0;
 			id = unique;
-			printf("%d\n", id);
 		}
 		if (now_loc == cnt) now_loc = 0; 
 		sum--;
