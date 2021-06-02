590cab7097d3b6797462896b3a91c0d70544f6c5
tracer-nju <tracer@nju.edu.cn>
2021-06-02 12:39:21 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: 9070006d94c14583a99285cfba1b93d6     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  12:39:21 up 1 day,  1:16,  6 users,  load average: 0.74, 0.78, 0.80

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -188,7 +188,7 @@ uint8_t* findClus(int loc, int sum, int id) {
 		for (int j = loc; j < sum; j++) val += pd(*start, line[j]), start = start + 1;
 		for (int j = 0; j < loc; j++)   val += pd(*start, line[j]), start = start + 1;
 		if (val < minn) {
-			minn = val, ans = (uint8_t *)(p + divided[3][i]);
+			minn = val, ans = (uint8_t *)(p + divided[3][i]); unique = i + 1;
 		}
 	}
 	assert (ans != NULL);
