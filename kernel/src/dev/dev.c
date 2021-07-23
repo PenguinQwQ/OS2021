01b3cd13913d8d65184e5cf44f7a97777aecad66
tracer-nju <tracer@nju.edu.cn>
2021-07-23 07:56:43 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: 9070006d94c14583a99285cfba1b93d6     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  07:56:43 up 5 days, 22:02,  6 users,  load average: 0.25, 0.23, 0.18

--- kernel/src/dev/dev.c
+++ kernel/src/dev/dev.c
@@ -12,9 +12,10 @@ device_t *devices[0 DEVICES(DEV_CNT)];
 
 static device_t *dev_lookup(const char *name) {
   for (int i = 0; i < LENGTH(devices); i++) {
+    if (strcmp(devices[i]->name, name) == 0) {
 	  printf("666\n");
-    if (strcmp(devices[i]->name, name) == 0)
       return devices[i];
+	}
   }
  printf("sasa\n");
   panic("lookup device failed.");
