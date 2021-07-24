7075d4bf353bfd3d98b6e231793f2212e533b35c
tracer-nju <tracer@nju.edu.cn>
2021-07-24 14:08:07 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: 9070006d94c14583a99285cfba1b93d6     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  14:08:07 up 6 days,  7:22,  6 users,  load average: 0.16, 0.27, 0.38

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -37,11 +37,10 @@ static void tty_reader(void *arg) {
 	   while (1) {
 		    tty->ops->write(tty, 0, ps, strlen(ps));
 			int nread = tty->ops->read(tty, 0, cmd, sizeof(cmd) - 1);
-		    cmd[nread] = '\0';
+		    cmd[nread - 1] = '\0';
 			if (cmd[0] == '0')printf("%d\n", vfs -> open(cmd + 2, O_CREAT));
 			else if (cmd[0] == '1')printf("%d\n", vfs -> chdir(cmd + 2));
 			else if (cmd[0] == '2')printf("%d\n", vfs -> close(atoi(cmd + 2)));
-		    sprintf(resp, "tty reader task: got %d character(s).\n", strlen(cmd));
 		    tty->ops->write(tty, 0, resp, strlen(resp));
 	  }
 }
