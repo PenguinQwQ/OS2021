87a26343931c61643391185f2220c2da95b96b71
tracer-nju <tracer@nju.edu.cn>
2021-07-24 09:33:23 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: 9070006d94c14583a99285cfba1b93d6     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  09:33:23 up 6 days,  4:39,  6 users,  load average: 0.48, 0.31, 0.33

--- kernel/include/vfs.h
+++ kernel/include/vfs.h
@@ -1,8 +1,17 @@
 #include <stdint.h>
 #include <common.h>
+#include <user.h>
 
 struct file {
 	char name[32];
 	uint32_t type, len, inode, NxtClus, count, size, bias, flag;
 }__attribute__((packed));
 
+
+struct fd_ {
+	int used;
+	int type;
+	int flag;
+	int loc;	
+	struct file* file;
+};
--- kernel/src/vfs.c
+++ kernel/src/vfs.c
@@ -1,6 +1,4 @@
-#include <common.h>
 #include <devices.h>
-#include <fcntl.h>
 #include <dirent.h>
 #include <vfs.h>
 #define MAX_CPU 8
@@ -10,6 +8,7 @@ device_t *sda;
 uint32_t current_dir[MAX_CPU];
 uint32_t mode[MAX_CPU];
 uint32_t *fat;
+struct fd_ fd[1024];
 
 uint32_t GetClusLoc(uint32_t clus) {
 	if (clus == 0) return 0;
@@ -28,9 +27,10 @@ static void vfs_init() {
 	for (int i = 0; i < MAX_CPU; i++)
 		current_dir[i] = 0x200000, mode[i] = 1;
 	assert(fat != NULL);
+	fd[0].used = fd[1].used = fd[2].used = 1;
 }
 
-uint32_t solve_path(uint32_t now, const char *path, int *status) {
+uint32_t solve_path(uint32_t now, const char *path, int *status, struct file *file) {
 	if (path[0] == 0) return now;
 	char *name = pmm -> alloc(256); ///////////////////////////////
 	int i = 0;
@@ -38,6 +38,7 @@ uint32_t solve_path(uint32_t now, const char *path, int *status) {
 	while (path[i] != 0 && path[i] != '/') 
 		name[i] = path[i], name[i + 1] = 0, i++;
 	path = path + i;
+	assert(strcmp("proc", name) && strcmp("dev", name)); ///////////////////////
 	void *tep = pmm -> alloc(4096); ///////////////////////////	
 	while (1) {
 		if (now == 0) break;
@@ -47,10 +48,11 @@ uint32_t solve_path(uint32_t now, const char *path, int *status) {
 			if (strcmp(name, nxt -> name) == 0) {
 				assert(nxt -> flag == 0); ///////////////////////////
 				if (nxt -> type == DT_DIR) 
-					return solve_path(GetClusLoc(nxt -> NxtClus), path, status);	
+					return solve_path(GetClusLoc(nxt -> NxtClus), path, status, file);	
 				else {
-					printf("%s assert!!\n", name);
-					assert(0); ///////////////////
+					if (path[0] != 0) return -1;
+					memcpy(file, nxt, sizeof(struct file));
+					return 0;
 				}					
 			}
 			nxt = nxt + 1;
@@ -67,7 +69,8 @@ static int vfs_chdir(const char *path) {
 	int status = (now == 0x200000) ? 1 : mode[id];
 	
 	assert(mode[id] == 1); ///////////////////////////////////////////
-	uint32_t nxt = solve_path(now, path + (path[0] == '/'), &status);
+	struct file tep;
+	uint32_t nxt = solve_path(now, path + (path[0] == '/'), &status, &tep);
 	int result = 0;
 	if (nxt == -1) result = -1;
 	else current_dir[id] = nxt;
@@ -76,7 +79,34 @@ static int vfs_chdir(const char *path) {
 	return result;
 }
 
+static int vfs_open(const char *path, int flags) {
+	kmt -> spin_lock(&vfs_lock);
+	int id = cpu_current();
+	uint32_t now = (path[0] == '/') ? 0x200000 : current_dir[id];
+	int status = (now == 0x200000) ? 1 : mode[id];
+	
+	assert(mode[id] == 1); ///////////////////////////////////////////
+
+	struct file* tep = pmm -> alloc(sizeof(struct file));
+	uint32_t nxt = solve_path(now, path + (path[0] == '/'), &status, tep);
+	int result = -1;
+	if (nxt == -1) result = -1;
+	else {
+		for (int i = 0; i < 1024; i++) 
+			if (fd[i].used == 0) {
+				fd[i].used = 1; 
+				fd[i].type = (nxt == 0) ? T_FILE : T_DIR; 
+				fd[i].flag = flags;	
+				result = i;
+				break;
+			}
+	}
+	kmt -> spin_unlock(&vfs_lock);	
+	return result;
+}
+
 MODULE_DEF(vfs) = {
 	.init  = vfs_init,	
 	.chdir = vfs_chdir,
+	.open  = vfs_open,
 };
