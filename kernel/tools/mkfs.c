14278309ce2cfcb8e860243fd9afc42a0bd79b1b
tracer-nju <tracer@nju.edu.cn>
2021-07-25 13:40:50 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: 9070006d94c14583a99285cfba1b93d6     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  13:40:50 up 6 days, 18:16,  7 users,  load average: 0.52, 0.21, 0.19

--- kernel/tools/mkfs.c
+++ kernel/tools/mkfs.c
@@ -116,7 +116,6 @@ void solve(DIR *dir, char *s, int spj) {
 }
 
 int main(int argc, char *argv[]) {
-  while(1);
   int fd, size = atoi(argv[1]) << 20;
 //  printf("%d\n", DT_DIR);
   assert((fd = open(argv[2], O_RDWR)) > 0);
