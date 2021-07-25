19d66de742451aa0182c23ee1e9edda94497ca59
tracer-nju <tracer@nju.edu.cn>
2021-07-25 12:43:52 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: 9070006d94c14583a99285cfba1b93d6     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  12:43:52 up 6 days, 17:19,  7 users,  load average: 0.24, 0.25, 0.26

--- kernel/src/vfs.c
+++ kernel/src/vfs.c
@@ -195,6 +195,7 @@ static int T = 0;
 static int vfs_open(const char *path, int flags) {
 	kmt -> spin_lock(&trap_lock);
 	T++;
+	if (T == 3)assert(0);
 	int id = cpu_current();
 	uint32_t now = (path[0] == '/') ? 0x200000 : current_dir[id];
 	int status = (now == 0x200000) ? 1 : mode[id];
