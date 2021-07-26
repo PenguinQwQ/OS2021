7c00112f4dc2561fbbf35dbecc64faa1fb312f2a
tracer-nju <tracer@nju.edu.cn>
2021-07-26 05:29:32 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 3c3d5ee778f349879722ca55752af1d0            Boot ID: 9070006d94c14583a99285cfba1b93d6     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  05:29:32 up 6 days, 20:37,  7 users,  load average: 0.23, 0.46, 0.31

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -69,7 +69,7 @@ static void tty_reader(void *arg) {
 	  }
 }
 */
-/*
+
 static void traverse(const char *root) {
   int sz = 4096;
   #define MAX_PATH_LEN 4096
@@ -399,14 +399,14 @@ vfs->chdir("..");
 traverse("");
 while(1);
 }
-*/
+
 static void os_init() {
   T++;
   Lists_sum = 0;
   pmm->init();
   kmt->init();
-//  dev -> init();
-//  vfs->init();
+  dev -> init();
+  vfs->init();
   kmt->spin_init(&trap_lock, "os_trap"); 
  // kmt->create(pmm -> alloc(sizeof(task_t)), "tty_reader", test, "tty1");
  // kmt->create(pmm -> alloc(sizeof(task_t)), "tty_reader", tty_reader, "tty1");
